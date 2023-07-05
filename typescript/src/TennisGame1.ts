ecbe1ff4b7cfa573630868ec2fc7c20add9286b2
Scott MESSNER <smessner@clever-age.com>
2023-07-05 12:23:05 +0200

    Refactoring of game1 with Strategy Pattern

--- typescript/src/TennisGame1.ts
+++ typescript/src/TennisGame1.ts
@@ -19,66 +19,82 @@ export class TennisGame1 implements TennisGame {
   }
 
   getScore(): string {
-    let score: string = '';
-    if (this.isDeuce()) {
-      score = 'Deuce';
-    } else if (this.player1Advantage()) {
-      score = 'Advantage ' + this.player1Name;
-    } else if (this.player2Advantage()) {
-      score = 'Advantage ' + this.player2Name;
-    } else if (this.player1Wins()) {
-      score = 'Win for ' + this.player1Name;
-    } else if (this.player2Wins()) {
-      score = 'Win for ' + this.player2Name;
-    } else if (this.isTiedAndNotDeuce()) {
-      score = this.getPlayer1Score() + '-All';
-    } else {
-      score = this.getPlayer1Score() + '-' + this.getPlayer2Score();
-    }
-    return score;
+    const strategies: ScoringStrategy[] = [
+      new DeuceStrategy(),
+      new Player1AdvantageStrategy(),
+      new Player2AdvantageStrategy(),
+      new Player1WinStrategy(),
+      new Player2WinStrategy(),
+      new TiedButNotDeuceStrategy(),
+      new DefaultScoringStrategy()
+    ]
+
+    let strategy: ScoringStrategy = strategies.filter(strategy => strategy.matches(this.player1Points, this.player2Points))[0];
+    return strategy.getScore(this.player1Name, this.player1Points, this.player2Name, this.player2Points);
+  }
+}
+
+interface ScoringStrategy {
+  matches(player1Points: number, player2Points: number): boolean;
+  getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string;
+}
+
+class DeuceStrategy implements ScoringStrategy {
+  public matches(player1Points: number, player2Points: number): boolean {
+    return player1Points === player2Points && player1Points >= 3;
   }
 
-  private pointsDifference() {
-    return this.player1Points - this.player2Points;
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return 'Deuce';
+  }
+}
+
+class Player1AdvantageStrategy implements ScoringStrategy {
+  public matches(player1Points: number, player2Points: number): boolean {
+    return (player1Points >= 4 || player2Points >= 4)
+      && player1Points - player2Points === 1;
   }
 
-  private player1Wins() {
-    return this.aPlayerHas4Points() && this.pointsDifference() >= 2;
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return 'Advantage ' + player1Name;
+  }
+}
+
+class Player2AdvantageStrategy implements ScoringStrategy {
+  public matches(player1Points: number, player2Points: number): boolean {
+    return (player1Points >= 4 || player2Points >= 4)
+      && player2Points - player1Points === 1;
   }
 
-  private player2Wins() {
-    return this.aPlayerHas4Points() && this.pointsDifference() <= 2;
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return 'Advantage ' + player2Name;
+  }
+}
+
+class Player1WinStrategy implements ScoringStrategy {
+  public matches(player1Points: number, player2Points: number): boolean {
+    return (player1Points >= 4 || player2Points >= 4)
+      && player1Points - player2Points >= 2;
   }
 
-  private player2Advantage() {
-    return this.aPlayerHas4Points() && this.pointsDifference() === -1;
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return 'Win for ' + player1Name;
+  }
+}
+
+class Player2WinStrategy implements ScoringStrategy {
+  public matches(player1Points: number, player2Points: number): boolean {
+    return (player1Points >= 4 || player2Points >= 4)
+      && player2Points - player1Points >= 2;
   }
 
-  private player1Advantage() {
-    return this.aPlayerHas4Points() && this.pointsDifference() === 1;
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return 'Win for ' + player2Name;
   }
+}
 
-  private aPlayerHas4Points() {
-    return this.player1Points >= 4 || this.player2Points >= 4;
-  }
-
-  private isDeuce() {
-    return this.player1Points === this.player2Points && this.player1Points >= 3;
-  }
-
-  private isTiedAndNotDeuce() {
-    return this.pointsDifference() === 0 && this.player1Points < 3;
-  }
-
-  private getPlayer1Score() {
-    return this.getPlayerScore(this.player1Points);
-  }
-
-  private getPlayer2Score() {
-    return this.getPlayerScore(this.player2Points);
-  }
-  
-  private getPlayerScore(points: number) {
+class RunningScoreStrategy {
+  public getPlayerScore(points: number): string {
     switch (points) {
       case 0:
         return 'Love';
@@ -89,5 +105,25 @@ export class TennisGame1 implements TennisGame {
       default:
         return 'Forty';
     }
+  }
+}
+
+class TiedButNotDeuceStrategy  extends RunningScoreStrategy implements ScoringStrategy{
+  public matches(player1Points: number, player2Points: number): boolean {
+    return player1Points - player2Points === 0 && player1Points < 3;
+  }
+
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return this.getPlayerScore(player1Points) + '-All';
+  }
+}
+
+class DefaultScoringStrategy extends RunningScoreStrategy implements ScoringStrategy {
+  public matches(player1Points: number, player2Points: number): boolean {
+    return player1Points <= 3 && player1Points <= 3 && player1Points != player2Points;
+  }
+
+  public getScore(player1Name: string, player1Points: number, player2Name: string, player2Points: number): string {
+    return this.getPlayerScore(player1Points) + '-' + this.getPlayerScore(player2Points);
   }
 }
